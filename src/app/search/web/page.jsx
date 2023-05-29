9a6a8801bb503e158759fa6909611a43f6197106
Durrell Jules <71855759+dujules23@users.noreply.github.com>
2023-05-29 15:54:58 -0400

    created search results component

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import Link from 'next/link'
+import WebSearchResults from '@/components/WebSearchResults'
 
 export default async function WebSearchPage({searchParams}) {
+  await new Promise((resolve) => setTimeout(resolve, 10000))
   const response = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
   )
@@ -28,7 +30,7 @@ export default async function WebSearchPage({searchParams}) {
   }
   return (
     <>
-      {results && results.map(result => <h1>{result.title}</h1>)}
+      {results && <WebSearchResults results={data} />}
     </>
   )
 }
