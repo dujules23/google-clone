9a41904a79bb3a1af2eadfeb7a2b1ccdaab4d966
Durrell Jules <71855759+dujules23@users.noreply.github.com>
2023-05-30 13:26:00 -0400

    updated country state to default value of United States

--- src/components/CountryLookup.jsx
+++ src/components/CountryLookup.jsx
@@ -3,7 +3,7 @@
 import React, { useState, useEffect } from 'react'
 
 export default function CountryLookup() {
-  const [country, setCountry] = useState("")
+  const [country, setCountry] = useState("United States")
 
   useEffect(() => {
     fetch(`https://ip-api.com/json/${process.env.NEXT_PUBLIC_IP_API_KEY}`)
